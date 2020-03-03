2cd2799a31b9eea9e0fd842ec642c29df0d68edc
5ft6 <manahoydt@gmail.com>
2020-03-03 22:27:13 +0000

    Add /fun with new foon items. HW 8

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ let items = ["Buy Food", "Prepare Food", "Cook Food", "Eat Food"];
 // set an empty array for new work items
 let workItems = ["Show Up", "Get Settled"];
 
+let foon = ["Eat", "Sleep", "Win"];
+
 // set EJS as the viewing engine to display html
 app.set('view engine', 'ejs');
 
@@ -52,6 +54,10 @@ app.post("/", function(req, res) {
 // display default to do list on the localhost:3000/work route!
 app.get("/work", function(req, res){
     res.render("list", {listTitle: "Work To Do List", newListItems: workItems})
+});
+
+app.get("/fun", function(req, res){
+    res.render("list", {listTitle: "Work To Do List", newListItems: foon})
 });
 
 app.listen(3000, function() {
